302102ae707500f6813746164e1da8efdb7ba8e8
pangjm <pangjm@xiaopeng.com>
2022-08-04 20:46:55 +0800

    为什么不使用push也能上去

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.mylibrary.MUtil;
@@ -10,6 +11,7 @@ import com.example.mylibrary.MUtil;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private Button mbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
